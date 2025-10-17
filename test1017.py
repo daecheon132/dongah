e6b870337a76cf8a2a7a86096fc007c77220e844
daecheon132 <ghslqpp0103@naver.com>
2025-10-17 10:06:36 +0900

    add dictionary
    딕셔너리 추가

--- test1017.py
+++ test1017.py
@@ -5,3 +5,9 @@ c = a + b
 print(c)
 d = ["khj", "kty"]
 print(d[1])
+e = {"name" : "hjk", "weight" : "67kg"}
+print(e)
+score = {'수학' : 90, '국어' : 100, '영어' : 87, '사회' : 79}
+print(score["사회"])
+
+print(score.keys())

d2d45259409fcf3b2d71668048484921f6ccb2ae
daecheon132 <ghslqpp0103@naver.com>
2025-10-17 10:18:39 +0900

    clean
    영역별로 분류

--- test1017.py
+++ test1017.py
@@ -1,13 +1,13 @@
 print("hello, world!")
+
 a = 5
 b = 3
 c = a + b
 print(c)
+
 d = ["khj", "kty"]
 print(d[1])
-e = {"name" : "hjk", "weight" : "67kg"}
-print(e)
+
 score = {'수학' : 90, '국어' : 100, '영어' : 87, '사회' : 79}
 print(score["사회"])
-
 print(score.keys())
